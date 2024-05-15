eb12a1764724089ddb5f031d1b3648b2e097e373
Van Tuijcom, Thomas <Thomas.VanTuijcom@delaware.pro>
2024-05-15 13:46:57 +0200

    Test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const keep_alive = require('./keep_alive.js')
 const { token } = require('./config.json');
 
 // Create a new client instance
