f08a7ac8c114f1dc610b0e8708e763e4f285f64c
Van Tuijcom, Thomas <Thomas.VanTuijcom@delaware.pro>
2024-05-15 14:05:13 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const keep_alive = require('./keep_alive.js')
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -15,26 +14,3 @@ client.once(Events.ClientReady, readyClient => {
 
 // Log in to Discord with your client's token
 client.login(token);
-/*
-----------------------------------------------------------------------------------------
-const Discord = require('discord.js'); //import client from discord
-const keep_alive = require('./keep_alive.js')
-
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  // Vérifier si le message n'est pas envoyé par le bot lui-même
-  if (msg.author.bot) return;
-
-  // Répondre "je suis là" lorsque l'utilisateur écrit "ici"
-  if (msg.content.toLowerCase() === 'ici') {
-    msg.reply('je suis là');
-  }
-});
-
-client.login(process.env.TOKEN); //login bot using token
-*/
